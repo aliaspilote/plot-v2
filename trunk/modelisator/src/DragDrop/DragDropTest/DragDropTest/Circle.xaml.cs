e90a366f4a4479f5c1250c5ff326bd434ad84464
qbonichot@gmail.com <qbonichot@gmail.com@e18376a6-4c4a-c4d8-49f2-e125df4c2990>
2015-06-24 08:32:49 +0000

    Ajout DragDropTest

---
+++ trunk/modelisator/src/DragDrop/DragDropTest/DragDropTest/Circle.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DragDropTest
+{
+    /// <summary>
+    /// Logique d'interaction pour Circle.xaml
+    /// </summary>
+    public partial class Circle : UserControl
+    {
+        private Brush _previousFill = null;
+        public Circle()
+        {
+            InitializeComponent();
+        }
+
+        public Circle (Circle c)
+        {
+            InitializeComponent();
+            this.circleUI.Height = c.circleUI.Height;
+            this.circleUI.Width = c.circleUI.Width;
+            this.circleUI.Fill = c.circleUI.Fill;
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            Mouse.SetCursor(Cursors.Hand);
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                // Empaquetage des données
+                DataObject data = new DataObject();
+                data.SetData(DataFormats.StringFormat, circleUI.Fill.ToString());
+                data.SetData("Double", circleUI.Height);
+                data.SetData("Object", this);
+
+                // Initialisation de l'opération de Drag&Drop
+                DragDrop.DoDragDrop(this, data, DragDropEffects.Copy | DragDropEffects.Move);
+            }
+        }
+
+        protected override void OnDrop(DragEventArgs e)
+        {
+            base.OnDrop(e);
+
+            // Si le DataObject contient des données, extraction
+            if(e.Data.GetDataPresent(DataFormats.StringFormat))
+            {
+                string dataString = (string)e.Data.GetData(DataFormats.StringFormat);
+
+                // Si la chaîne peuet être convertie en Brush, conversion et application à l'ellipse
+                BrushConverter converter = new BrushConverter();
+                if (converter.IsValid(dataString))
+                {
+                    Brush newFill = (Brush)converter.ConvertFromString(dataString);
+                    circleUI.Fill = newFill;
+
+                    // Marque l'évènementDrop comme étant géré. On doit marquer l'évènement de déplacement comme étant géré afin que les autres
+                    // élements qui reçoivent cet évèement sachent que le contrôle utilisateur Circle l'a traité
+                    if (e.KeyStates.HasFlag(DragDropKeyStates.ControlKey))
+                    {
+                        e.Effects = DragDropEffects.Copy;
+                    }
+                    else
+                    {
+                        e.Effects = DragDropEffects.Move;
+                    }
+                }
+            }
+            e.Handled = true;
+        }
+
+        protected override void OnGiveFeedback(GiveFeedbackEventArgs e)
+        {
+            base.OnGiveFeedback(e);
+            if (e.Effects.HasFlag(DragDropEffects.Copy) || e.Effects.HasFlag(DragDropEffects.Move))
+            {
+                Mouse.SetCursor(Cursors.Cross);
+            }
+            else
+            {
+                Mouse.SetCursor(Cursors.No);
+            }
+            e.Handled = true;
+        }
+
+        protected override void OnDragEnter(DragEventArgs e)
+        {
+            base.OnDragEnter(e);
+            _previousFill = circleUI.Fill;
+        }
+    }
+}
